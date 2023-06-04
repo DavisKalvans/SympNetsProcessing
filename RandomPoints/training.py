aa09ef622c07eae55028fd37c6c6f432a29205f6
Davis Kalvans <davis.kalvans@gmail.com>
2023-06-04 13:11:39 +0300

    Comments/fixes

--- RandomPoints/training.py
+++ RandomPoints/training.py
@@ -11,8 +11,10 @@ tau = 0.01 # Time step
 nL = 2 # Layers
 nN = 4 # Width of each layer
 nM = 0 # Seed for model parameter initialization
-epochs = 100_000
+epochs = 1_000 # Only use thousands here
 extraParams = None # Extra parameters for problems that need it, eg, HarmOsc has omega; None by default
+
+### Areas for mult predictions/convergence; starting points for one trajectory prediction/convergence
 x0_HarmOsc = (0.3, 0.5)
 x0_Pendulum = (0.8, 0.5)
 e = 0.6
@@ -37,30 +39,35 @@ elif problem == "Pendulum":
     x0 = x0_Pendulum
     area = area_Pendulum
 
-linear = False
+linear = False # Use a linear activation function
 learning_rate = 1e-2
-sch = False
-eta1 = 1e-1
-eta2 = 1e-2
+sch = False # Use scheduling for learning rate
+eta1 = 1e-1 # For scheduling (starting learning rate)
+eta2 = 1e-2 # For scheduling (ending learning rate)
 
 data = (N, M, tau) # Training data N, testing data M, time step tau
 
 trainParams = (learning_rate, N, epochs, sch, eta1, eta2, linear) # Learning rate, batch size, epochs, scheduling, eta1, eta2, linear
 predParams = (1000, tau, x0, False) # Time steps, x0, best
 convParams = (x0, 10, 0, 5, dim, False) # x0, Tend, lower power, upper power, dim, best
-predParamsMult = (1000, tau, area, 0, 100, False) # Time steps, area for starting points, timestep to use, seed, 
+predParamsMult = (1000, tau, area, 0, 10, False) # Time steps, area for starting points, timestep to use, seed, 
 #nr of trajectories, use the best model or not
-convParamsMult = (10, 0, 5, dim, area, 0, 100, False) # Tend, lower power, upper power, area for starting points,
+convParamsMult = (10, 0, 5, dim, area, 0, 10, False) # Tend, lower power, upper power, area for starting points,
 #seed, nr of trajectories, use the best model or not
 
 # Loop trough combinations of layer count and their width
-for nL in [2]:
-    for nN in [4]:
-        for nM in [0]:
-            #general_training.train_model(problem, method, device, data, trainParams, nL, nN, nM, extraParams)
-            #general_training.model_errors(problem, method, device, data, trainParams, nL, nN, nM, predParams, extraParams)
-            #general_training.model_conv(problem, method, device, data, trainParams, nL, nN, nM, convParams, extraParams)
-            #general_training.model_multTrajectErrors(problem, method, device, data, trainParams, nL, nN, nM, predParamsMult, extraParams)
+for nL in [2]: # Layers
+    for nN in [4]: # Width
+        for nM in [0, 1]: # Seeds to use
+            # Train model
+            general_training.train_model(problem, method, device, data, trainParams, nL, nN, nM, extraParams)
+            # Calculate and plot prediction errors for one specific trajectory x0
+            general_training.model_errors(problem, method, device, data, trainParams, nL, nN, nM, predParams, extraParams)
+            # Calculate and plot prediction error convergence for one specific trajectory x0
+            general_training.model_conv(problem, method, device, data, trainParams, nL, nN, nM, convParams, extraParams)
+            # Calculate and plot prediction error average for many trajectories
+            general_training.model_multTrajectErrors(problem, method, device, data, trainParams, nL, nN, nM, predParamsMult, extraParams)
+            # Calculate and plot prediction error convergence average for many trajectories
             general_training.model_multTrajectConv(problem, method, device, data, trainParams, nL, nN, nM, convParamsMult, extraParams)
 
             # Do everything with best model too
--- RandomPoints/trainingSym.py
+++ RandomPoints/trainingSym.py
@@ -2,20 +2,20 @@ import general_trainingVerlSym
 import numpy as np
 import torch
 
-torch.backends.cuda.matmul.allow_tf32 = False
-
 ### HarmOsc Verlet settings
 problem = 'Kepler'
 method = 'Verlet'
 device = 'cpu'
 N = 40 # Training data
 M = 100 # Testing data
-tau = 0.1 # Time step
+tau = 0.01 # Time step
 nL = 2 # Layers
 nN = 4 # Width of each layer
 nM = 0 # Seed for model parameter initialization
-epochs = 100_000
+epochs = 1_000 # Only use thousands here
 extraParams = None # Extra parameters for problems that need it, eg, HarmOsc has omega; None by default
+
+### Areas for mult predictions/convergence; starting points for one trajectory prediction/convergence
 x0_HarmOsc = (0.3, 0.5)
 x0_Pendulum = (0.8, 0.5)
 e = 0.6
@@ -40,40 +40,42 @@ elif problem == "Pendulum":
     x0 = x0_Pendulum
     area = area_Pendulum
 
-linear = False
+linear = False # Use a linear activation function
 learning_rate = 1e-2
-sch = False
-eta1 = 1
-eta2 = 1e-1
+sch = False # Use scheduling for learning rate
+eta1 = 1 # For scheduling (starting learning rate)
+eta2 = 1e-1 # For scheduling (ending learning rate)
 
 data = (N, M, tau) # Training data N, testing data M, time step tau
 trainParams = (learning_rate, N, epochs, sch, eta1, eta2, linear) # Learning rate, batch size, epochs, scheduling, eta1, eta2, linear
 predParams = (1000, tau, x0, False) # Time steps, x0, best
 convParams = (x0, 10, 0, 5, dim, False) # x0, Tend, lower power, upper power, dim, best
-predParamsMult = (1000, tau, area, 0, 100, False) # Time steps, area for starting points, timestep to use, seed, 
+predParamsMult = (1000, tau, area, 0, 10, False) # Time steps, area for starting points, timestep to use, seed, 
 #nr of trajectories, use the best model or not
-convParamsMult = (100, 0, 5, dim, area, 0, 100, False) # Tend, lower power, upper power, area for starting points,
+convParamsMult = (10, 0, 5, dim, area, 0, 10, False) # Tend, lower power, upper power, area for starting points,
 #seed, nr of trajectories, use the best model or not
 
 # Loop trough combinations of layer width
-# for nM in [8]:
-#     for nL in [2]:
-#         for nN in [32]:
-#             #general_trainingVerlSym.train_model(problem, method, device, data, trainParams, nL, nN, nM, extraParams)
-#             #general_trainingVerlSym.model_errors(problem, method, device, data, trainParams, nL, nN, nM, predParams, extraParams)
-#             #general_trainingVerlSym.model_conv(problem, method, device, data, trainParams, nL, nN, nM, convParams, extraParams)
-#             #general_trainingVerlSym.model_multTrajectErrors(problem, method, device, data, trainParams, nL, nN, nM, predParamsMult, extraParams)
-#             general_trainingVerlSym.model_multTrajectConv(problem, method, device, data, trainParams, nL, nN, nM, convParamsMult, extraParams)
+for nM in [2]: # Layers
+    for nL in [4]: # Width
+        for nN in [0, 1]: # Seeds to use
+            # Train model
+            general_trainingVerlSym.train_model(problem, method, device, data, trainParams, nL, nN, nM, extraParams)
+            # Calculate and plot prediction errors for one specific trajectory x0
+            general_trainingVerlSym.model_errors(problem, method, device, data, trainParams, nL, nN, nM, predParams, extraParams)
+            # Calculate and plot prediction error convergence for one specific trajectory x0
+            general_trainingVerlSym.model_conv(problem, method, device, data, trainParams, nL, nN, nM, convParams, extraParams)
+            # Calculate and plot prediction error average for many trajectories
+            general_trainingVerlSym.model_multTrajectErrors(problem, method, device, data, trainParams, nL, nN, nM, predParamsMult, extraParams)
+            # Calculate and plot prediction error convergence average for many trajectories
+            general_trainingVerlSym.model_multTrajectConv(problem, method, device, data, trainParams, nL, nN, nM, convParamsMult, extraParams)
 
-#             # Do everything with best model too
-#             #predParams_best = (1000, x0_Pendulum, True) # Time steps, x0, best
-#             #convParams_best = (x0_Pendulum, 10, 0, 5, dim, True) # x0, Tend, lower power, upper power, dim, best
-#             #predParamsMult_best = (1000, tau, area_Pendulum, 0, 10, True)
-#             #convParamsMult_best = (100, 0, 5, dim, area_Pendulum, 0, 10, False) # Tend, lower power, upper power, area for starting points,
-#             #general_trainingVerlSym.model_errors(problem, method, device, data, trainParams, nL, nN, nM, plotParams_best, extraParams)
-#             #general_trainingVerlSym.model_conv(problem, method, device, data, trainParams, nL, nN, nM, convParams_best, extraParams)
-#             #general_trainingVerlSym.model_multTrajectErrors(problem, method, device, data, trainParams, nL, nN, nM, predParamsMult_best, extraParams)
-#             #general_trainingVerlSym.model_multTrajectConv(problem, method, device, data, trainParams, nL, nN, nM, convParamsMult_best, extraParams)
-
-for nM, nL, nN in [[0, 2, 64], [0, 2, 128], [0, 4, 64], [0, 4, 128], [0, 8, 64], [0, 8, 128]]:
-    general_trainingVerlSym.train_model(problem, method, device, data, trainParams, nL, nN, nM, extraParams)
+            # Do everything with best model too
+            #predParams_best = (1000, x0_Pendulum, True) # Time steps, x0, best
+            #convParams_best = (x0_Pendulum, 10, 0, 5, dim, True) # x0, Tend, lower power, upper power, dim, best
+            #predParamsMult_best = (1000, tau, area_Pendulum, 0, 10, True)
+            #convParamsMult_best = (100, 0, 5, dim, area_Pendulum, 0, 10, False) # Tend, lower power, upper power, area for starting points,
+            #general_trainingVerlSym.model_errors(problem, method, device, data, trainParams, nL, nN, nM, plotParams_best, extraParams)
+            #general_trainingVerlSym.model_conv(problem, method, device, data, trainParams, nL, nN, nM, convParams_best, extraParams)
+            #general_trainingVerlSym.model_multTrajectErrors(problem, method, device, data, trainParams, nL, nN, nM, predParamsMult_best, extraParams)
+            #general_trainingVerlSym.model_multTrajectConv(problem, method, device, data, trainParams, nL, nN, nM, convParamsMult_best, extraParams)
