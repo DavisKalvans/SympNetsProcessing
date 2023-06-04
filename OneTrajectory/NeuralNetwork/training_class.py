30c2e3cf32a779d4d5e557806617c6396d707954
Davis Kalvans <davis.kalvans@gmail.com>
2023-06-04 12:07:54 +0300

    Remove old train and test loops

--- OneTrajectory/NeuralNetwork/training_class.py
+++ OneTrajectory/NeuralNetwork/training_class.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 
 # https://discuss.pytorch.org/t/vanishing-gradients/46824/7
+# Can be used to check for gradient vanishing/exploding
 def plot_grad_flow(named_parameters):
     '''Plots the gradients flowing through different layers in the net during training.
     Can be used for checking for possible gradient vanishing / exploding problems.
@@ -19,7 +20,7 @@ def plot_grad_flow(named_parameters):
 
     return ave_grads, max_grads, layers
 
-
+# Training epoch with nonsymetrical models (k=1)
 def train_loopGeneral(numeric_step, dataloader, model, loss_fn, optimizer, scheduler, sch, params = None):
     for batch, (X, y, tau) in enumerate(dataloader):
         inverse, _ = model.back(X, tau) # Pass trough inverse model
@@ -41,6 +42,7 @@ def train_loopGeneral(numeric_step, dataloader, model, loss_fn, optimizer, sched
     
     return error
 
+# Validation with nonsymetrical models (k=1)
 def test_loopGeneral(numeric_step, dataloader, model, loss_fn, params = None):
 
     batches = len(dataloader)
@@ -60,6 +62,7 @@ def test_loopGeneral(numeric_step, dataloader, model, loss_fn, params = None):
 
     return loss
 
+# Training epoch with symetrical models (k=2)
 def train_loopGeneralSym(numeric_step, dataloader, model, loss_fn, optimizer, scheduler, sch, params = None):
     for X, y, tau in dataloader:
         inverse, _ = model.back(X, torch.pow(tau, 2)) # Pass trough inverse model
@@ -81,6 +84,7 @@ def train_loopGeneralSym(numeric_step, dataloader, model, loss_fn, optimizer, sc
     
     return error
 
+# Validation with symetrical models (k=2)
 def test_loopGeneralSym(numeric_step, dataloader, model, loss_fn, params = None):
     with torch.no_grad():
         for X, y , tau in dataloader:
@@ -95,216 +99,3 @@ def test_loopGeneralSym(numeric_step, dataloader, model, loss_fn, params = None)
 
     return loss
 
-def train_loop(dataloader, model, omega, loss_fn, optimizer, scheduler, sch):
-
-    for batch, (X, y, tau) in enumerate(dataloader):
-        inverse, _ = model.back(X, tau) # Pass trough inverse model
-        
-        # Need to do the numerical method now (symplectic Euler)
-        XX = torch.zeros((X.size(dim=0), X.size(dim=1), X.size(dim=2)), dtype=torch.float32)
-            
-        
-        a = inverse[:, 0, 1] - omega**2*tau.T*inverse[:, 0, 0]
-        b = inverse[:, 0, 0] + a[0,  :]*tau.T
-        a = a.reshape((1, 1, X.size(dim=0)))
-        XX[:, 0, 1] = a
-        XX[:, 0, 0] = b
-        
-        pred, _ = model(XX, tau) # Pass trough original model
-        loss = loss_fn(pred, y)
-
-        error = loss.detach()
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-
-        # Debug for cheking gradients when there is afear of vanishing/exploding gradients
-        #grads = plot_grad_flow(model.named_parameters())
-        optimizer.step()
-        
-        # Scheduler
-        if sch:
-            scheduler.step()
-    
-    return error
-
-def test_loop(dataloader, model, omega, loss_fn):
-
-    batches = len(dataloader)
-    loss = 0
-    with torch.no_grad():
-        for batch, (X, y , tau) in enumerate(dataloader):
-            inverse, _ = model.back(X, tau) # Pass trough inverse model
-        
-            # Need to do the numerical method now (symplectic Euler)
-            XX = torch.zeros((X.size(dim=0), X.size(dim=1), X.size(dim=2)), dtype=torch.float32)
-            
-            a = inverse[:, 0, 1] -omega**2*tau.T*inverse[:, 0, 0]
-            b = inverse[:, 0, 0] + a[0,  :]*tau.T
-            a = a.reshape((1, 1, X.size(dim=0)))
-            XX[:, 0, 1] = a
-            XX[:, 0, 0] = b
-
-            pred, _ = model(XX, tau) # Pass trough original model
-            loss += loss_fn(pred, y).item()
-
-    loss /= batches
-
-    return loss
-
-def train_loop_pendulum(dataloader, model, loss_fn, optimizer, scheduler, sch):
-
-    for batch, (X, y, tau) in enumerate(dataloader):
-        inverse, _ = model.back(X, tau) # Pass trough inverse model
-        
-        # Need to do the numerical method now (symplectic Euler)
-        XX = torch.zeros((X.size(dim=0), X.size(dim=1), X.size(dim=2)), dtype=torch.float32)
-        
-        a = inverse[:, 0, 1] -tau.T*torch.sin(inverse[:, 0, 0])
-        b = inverse[:, 0, 0] + a[0,  :]*tau.T
-        a = a.reshape((1, 1, X.size(dim=0)))
-        XX[:, 0, 1] = a
-        XX[:, 0, 0] = b
-        
-        pred, _ = model(XX, tau) # Pass trough original model
-        loss = loss_fn(pred, y)
-
-        error = loss.detach()
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-
-        # Debug for cheking gradients when there is afear of vanishing/exploding gradients
-        #grads = plot_grad_flow(model.named_parameters())
-        optimizer.step()
-        
-        # Scheduler
-        if sch:
-            scheduler.step()
-    
-    return error
-
-def test_loop_pendulum(dataloader, model, loss_fn):
-
-    batches = len(dataloader)
-    loss = 0
-    with torch.no_grad():
-        for batch, (X, y, tau) in enumerate(dataloader):
-            inverse, _ = model.back(X, tau) # Pass trough inverse model
-        
-            # Need to do the numerical method now (symplectic Euler)
-            XX = torch.zeros((X.size(dim=0), X.size(dim=1), X.size(dim=2)), dtype=torch.float32)
-            
-            a = inverse[:, 0, 1] -tau.T*torch.sin(inverse[:, 0, 0])
-            b = inverse[:, 0, 0] + a[0,  :]*tau.T
-            a = a.reshape((1, 1, X.size(dim=0)))
-            XX[:, 0, 1] = a
-            XX[:, 0, 0] = b
-
-            pred, _ = model(XX, tau) # Pass trough original model
-            loss += loss_fn(pred, y).item()
-
-    loss /= batches
-
-    return loss
-
-def train_loop_kepler(dataloader, model, loss_fn, optimizer, scheduler, sch):
-
-    for batch, (X, y, tau) in enumerate(dataloader):
-        inverse, _ = model.back(X, tau) # Pass trough inverse model
-        
-        # Need to do the numerical method now (symplectic Euler)
-        XX = torch.zeros((X.size(dim=0), X.size(dim=1), X.size(dim=2)), dtype=torch.float32)
-        tau_vec = tau.reshape(tau.size(0))
-            
-        q1 = inverse[:, 0, 0] +tau_vec*inverse[:, 0, 2]
-        q2 = inverse[:, 0, 1] +tau_vec*inverse[:, 0, 3]
-        d = torch.pow(torch.pow(q1, 2) +torch.pow(q2, 2), 1.5)
-        p1 = inverse[:, 0, 2] -tau_vec*q1/d
-        p2 = inverse[:, 0, 3] -tau_vec*q2/d
-
-        XX[:, 0, 0] = q1
-        XX[:, 0, 1] = q2
-        XX[:, 0, 2] = p1
-        XX[:, 0, 3] = p2
-
-        pred, _ = model(XX, tau) # Pass trough original model
-        loss = loss_fn(pred, y)
-
-        error = loss.detach()
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-
-        # Debug for cheking gradients when there is afear of vanishing/exploding gradients
-        #grads = plot_grad_flow(model.named_parameters())
-        optimizer.step()
-        
-        # Scheduler
-        if sch:
-            scheduler.step()
-    
-    return error
-
-def test_loop_kepler(dataloader, model, loss_fn):
-
-    batches = len(dataloader)
-    loss = 0
-    with torch.no_grad():
-        for batch, (X, y , tau) in enumerate(dataloader):
-            inverse, _ = model.back(X, tau) # Pass trough inverse model
-        
-            # Need to do the numerical method now (symplectic Euler)
-            XX = torch.zeros((X.size(dim=0), X.size(dim=1), X.size(dim=2)), dtype=torch.float32)
-            tau_vec = tau.reshape(tau.size(0))
-                
-            q1 = inverse[:, 0, 0] +tau_vec*inverse[:, 0, 2]
-            q2 = inverse[:, 0, 1] +tau_vec*inverse[:, 0, 3]
-            d = torch.pow(torch.pow(q1, 2)+torch.pow(q2, 2), 1.5)
-            p1 = inverse[:, 0, 2] -tau_vec*q1/d
-            p2 = inverse[:, 0, 3] -tau_vec*q2/d
-
-            XX[:, 0, 0] = q1
-            XX[:, 0, 1] = q2
-            XX[:, 0, 2] = p1
-            XX[:, 0, 3] = p2
-
-            pred, _ = model(XX, tau) # Pass trough original model
-            loss += loss_fn(pred, y).item()
-
-    loss /= batches
-
-    return loss
-
-def train_loop_classic(dataloader, model, loss_fn, optimizer, scheduler, sch):
-
-    for batch, (X, y, tau) in enumerate(dataloader): 
-        pred, _ = model(X, tau) # Pass trough original model
-        loss = loss_fn(pred, y)
-
-        error = loss.detach()
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        # Debug for cheking gradients when there is afear of vanishing/exploding gradients
-        #grads = plot_grad_flow(model.named_parameters())
-        optimizer.step()
-        
-        # Scheduler
-        if sch:
-            scheduler.step()
-    
-    return error
-
-def test_loop_classic(dataloader, model, loss_fn):
-
-    batches = len(dataloader)
-    loss = 0
-    with torch.no_grad():
-        for batch, (X, y , tau) in enumerate(dataloader):
-            pred, _ = model(X, tau) # Pass trough original model
-            loss += loss_fn(pred, y).item()
-
-    loss /= batches
-
-    return loss
